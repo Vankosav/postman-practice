cf19a6abb04b420c9e7044ef279bfb7616445276
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2024-02-16 04:12:09 +0100

    full crud app

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 
 
 const User  = mongoose.model ('CRUD', {
-  name: String,
-  lastname: String
+  username: String,
+  email: String
 });
 
 router.get('/', async (req, res) => {
@@ -24,6 +24,19 @@ router.get('/createUser', (req, res) => {
 router.post("/createUser", async (req, res) => {
   const newUser = req.body;
   await User.create(newUser);
+  res.send('completed');
+})
+
+router.put('/updateUser/:id', async (req, res) => {
+  const { id } = req.params;
+  const { username, email } = req.body;
+  await User.findByIdAndUpdate (id, { username, email }); 
+  res.send('completed');
+})
+
+router.delete('/deleteUser/:id', async (req, res) => {
+  const { id } = req.params;
+  await User.findByIdAndDelete(id);
   res.send('completed');
 })
 
